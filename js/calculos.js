4ffbe1525849117e284dc4a7901586902d10de77
zkadminster <kerry.guzman12@gmail.com>
2025-08-21 19:06:30 -0400

    Add files via upload

--- js/calculos.js
+++ js/calculos.js
@@ -6,6 +6,15 @@
     let modalidad = document.getElementById("mod")
     let portadaop = document.getElementById("portop")
     let st3s = document.getElementById("st3")
+    let dt = document.getElementById("fact")
+    let ivas =  document.getElementById("iva")
+    let validez = document.getElementById("valycont")
+    let respaldos = document.getElementById("respal")
+    let ct = document.getElementById("ct")
+    let cot = document.getElementById("coti")
+    let dec = document.getElementById("dj")
+    let rd = document.getElementById("rd")
+    let a5 = document.getElementById("a5")
 
     let comentario_final = document.getElementById("resultado")
 
@@ -31,18 +40,80 @@ function comentario(){
         var est_portop= ""  
     } 
 
-    // COMENTARIO PORTADA DE OP
+    // COMENTARIO SITUACIÓN TRIBUTARIA DE TERCEROS
     if (st3s.value != ""){
         var est_st3= ", considerar que la situación tributaria de terceros " + st3s.value
     } else if (portadaop.value == ""){
         var est_st3= ""  
     } 
 
-console.log(tipo + est_op + est_portop + est_st3) 
+    // DOCUMENTO TRIBUTARIO ( FACTURA - BOLETA DE HONORARIOS )
+    if (dt.value != ""){
+        var est_dt = ", en relación con el documento tributario " + dt.value 
+    } else if (dt.value == ""){
+        var est_dt = ""  
+    } 
+
+    // ESTADO PAGO IVA
+    if (ivas.value != ""){
+        var est_iva = ", en cuanto al pago de iva "  + ivas.value 
+    } else if (ivas.value == ""){
+        var est_iva = ""  
+    }    
+
+    // VALIDEZ Y CONTENIDO
+    if (validez.value != ""){
+        var est_valid =", acerca de la validez y contenido "   + validez.value 
+    } else if (validez.value == ""){
+        var est_valid = ""  
+    }   
+
+
+    // RESPALDOS
+    if (respaldos.value != ""){
+        var est_respald =", respecto de los respaldos " + respaldos.value 
+    } else if (respaldos.value == ""){
+        var est_respald = ""  
+    }   
+
+    // PORTADA CARPETA TRIBUTARIA
+    if (ct.value != ""){
+        var est_ct =", en la portada de carpeta tributaria " + ct.value 
+    } else if (ct.value == ""){
+        var est_ct = ""  
+    }   
+
+    // COTIZACIÓN
+    if (cot.value != ""){
+        var est_cot = ", en cuanto a la cotización "  + cot.value 
+    } else if (cot.value == ""){
+        var est_cot = ""  
+    }   
+
+    // DECLARACIÓN JURADA
+    if (dec.value != ""){
+        var est_dec = ", acerca de la declaración jurada "   + dec.value 
+    } else if (dec.value == ""){
+        var est_dec = ""  
+    }   
+
+    // RECEPCIÓN DE DOCUMENTOS
+    if (rd.value != ""){
+        var est_rd = ", respecto de la recepción de documentos "   + rd.value 
+    } else if (rd.value == ""){
+        var est_rd = ""  
+    }   
+
+    // ANEXO 5
+    if (a5.value != ""){
+        var est_a5 =  ", en relación al anexo 5 "   + a5.value 
+    } else if (a5.value == ""){
+        var est_a5 = ""  
+    }   
 
 
 
-comentario_final.value = (tipo + est_op + est_portop + est_st3) 
+comentario_final.value = (tipo + est_op + est_portop + est_st3 + est_dt + est_iva + est_valid + est_respald + est_ct + est_cot + est_dec + est_rd + est_a5) 
 }
 
 inicio.addEventListener('click',comentario)
